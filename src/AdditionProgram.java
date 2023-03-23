51513ea452d403154e9c183341bb8dad9de3dc6f
143ni <143ni@Nikhil>
2023-03-23 12:05:33 +0530

    First Commit

---
+++ src/AdditionProgram.java
@@ -0,0 +1,12 @@
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;
+		
+		System.out.println("Addition = " + num1 + num2);
+	}
+
+}
---
+++ src/programs/AdditionProgram.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;
+		int total = num1 + num2;
+		System.out.println("Addition = " + total);
+	}
+
+}
